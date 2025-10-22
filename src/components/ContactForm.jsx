d14fc28d65269263306923df9b4bacdedf10895d
youssefBelamine <youbl4382@gmail.com>
2025-10-22 21:07:50 +0100

    update

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,16 +1,23 @@
 import React from "react";
+import { useForm, ValidationError } from '@formspree/react';
 
 export function ContactForm() {
+  const [state, handleSubmit] = useForm("mjkaqokv");
+  // if (state.succeeded) {
+  //     return <p>Thanks for joining!</p>;
+  // }
+  console.log(state.submitting)
   return (
     <div className=" p-6 rounded-lg shadow-md w-full
              border backdrop-blur-md text-white">
       {/* className="flex-1 p-8 rounded-lg shadow-lg 
              bg-indigo-900/70 backdrop-blur-md text-white" */}
-      <h2 className="text-2xl font-bold mb-6 text-center md:text-left">Contact Me</h2>
-      <form action="https://formspree.io/f/mjkaqokv"
-  method="POST" className="flex flex-col gap-4">
+      <h2 className="text-2xl font-bold mb-6 text-center md:text-center bg-white text-black rounded-full px-3">Contact Me</h2>
+      <form  onSubmit={handleSubmit} className="flex flex-col gap-4">
         <div className="flex flex-col">
-          <label htmlFor="name" className="mb-1 font-medium">Full Name</label>
+
+                                    {/* Name field */}
+          <label htmlFor="name" className="mb-1 font-medium"> <span className="bg-white text-black rounded-full px-3 ml-1">Name</span> </label>
           <input
             type="text"
             id="name"
@@ -21,8 +28,9 @@ export function ContactForm() {
           />
         </div>
 
+                                    {/* Email field */}
         <div className="flex flex-col">
-          <label htmlFor="email" name="_replyto" required className="mb-1 font-medium">Email</label>
+          <label htmlFor="email" className="mb-1 font-medium"><span className="bg-white text-black rounded-full px-3 ml-1 w-1/4">Email</span></label>
           <input
             type="email"
             id="email"
@@ -31,22 +39,33 @@ export function ContactForm() {
             required
             className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
+          <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
         </div>
 
+                                    {/* Subject Field */}
         <div className="flex flex-col">
-          <label htmlFor="subject" className="mb-1 font-medium">Subject</label>
-          <textarea
+          <label htmlFor="subject" className="mb-1 font-medium"><span className="bg-white text-black rounded-full px-3 ml-1 w-1/4">Subject</span></label>
+          <input
             id="subject"
             name="subject"
             rows="2"
             placeholder="Your subject"
-            required
             className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          ></textarea>
+          />
+          <ValidationError 
+        prefix="Subject" 
+        field="subject"
+        errors={state.errors}
+      />
         </div>
 
+                                    {/* message Field */}
         <div className="flex flex-col">
-          <label htmlFor="message" className="mb-1 font-medium">Message</label>
+          <label htmlFor="message" className="mb-1 font-medium"><span className="bg-white text-black rounded-full px-3 ml-1 w-1/4">Message</span></label>
           <textarea
             id="message"
             name="message"
@@ -55,11 +74,17 @@ export function ContactForm() {
             required
             className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           ></textarea>
+          <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
         </div>
-
+                                            {/* Submit Button */}
         <button
           type="submit"
-          className="mt-4 border w-1/3 m-auto text-white hover:text-black font-semibold py-2 rounded-md hover:bg-white transition-colors"
+           disabled={state.submitting}
+          className={` ${state.submitting ? "text-gray-400 hover:text-black hover:bg-gray-400" : "text-white hover:text-black hover:bg-white"} mt-4 border w-1/3 m-auto font-semibold py-2 rounded-md transition-colors`}
         >
           Send Message
         </button>
