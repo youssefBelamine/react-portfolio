beb277e8d22d0f778264ad481e432b29606fb2f3
youssefBelamine <youbl4382@gmail.com>
2025-10-25 22:09:37 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Example from './components/Example.jsx'
-import { ContactForm } from './components/ContactForm.jsx'
-// import Index from './components/NewcontactForm.jsx'
 
 createRoot(document.getElementById('root')).render(
   <>
