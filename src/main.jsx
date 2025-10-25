1d93e9125c9fe16c5d1ccf76d38d8be4d4d331a4
youssefBelamine <youbl4382@gmail.com>
2025-10-25 22:07:49 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import Example from './components/Example.jsx'
 import { ContactForm } from './components/ContactForm.jsx'
-import Index from './components/NewcontactForm.jsx'
+// import Index from './components/NewcontactForm.jsx'
 
 createRoot(document.getElementById('root')).render(
   <>
